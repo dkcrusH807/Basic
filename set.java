8e10721417d079a3f2bb7cf975b784e9d43bb1a1
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-12 17:36:27 +0530

    Add files via upload

---
+++ set.java
@@ -0,0 +1,14 @@
+package set1;
+import java.util.*;
+public class Set1 {
+public static void main(String[] args) {
+	int N;
+	Scanner in=new Scanner(System.in);
+	N=in.nextInt();
+	if(N==0){
+		System.out.println("Zero");
+	}
+	else if(N<0){
+		System.out.println("Negative");}
+	else if(N>0){
+		System.out.println("Positve");}}}
