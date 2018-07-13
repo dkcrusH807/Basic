d72ac3c9650a730f630cb1d295ecf173de791624
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-13 09:53:36 +0530

    Add files via upload

---
+++ power.java
@@ -0,0 +1,11 @@
+package set1;
+import java.util.*;
+public class Set1 {
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+double n,p;
+n=in.nextDouble();
+p=in.nextDouble();
+System.out.println(Math.pow(n, p));
+}
+}
