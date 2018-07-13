f4d33a459ca0b7131280837012e476fb6f0686e0
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-13 10:20:11 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,18 @@
+package set1;
+import java.util.*;
+public class Set1 {
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int n,count=0;
+n=in.nextInt();
+for(int i=1;i<n;i++){
+	if(n%i==0)
+		count++;
+}
+if(count==1){
+	System.out.println("yes");
+}else{
+	System.out.println("no");
+}
+}
+}
