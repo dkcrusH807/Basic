31d092212052328fd3cad77e10c4fc9891f9644f
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-13 09:24:38 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,12 @@
+package set1;
+import java.util.*;
+public class Set1 {
+public static void main(String[] args) {
+int n;
+Scanner in=new Scanner(System.in);
+n=in.nextInt();
+for(int i=0;i<n;i++){
+	System.out.println("Hello");
+}
+}
+}
