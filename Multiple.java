85962b404a3d233e1ad8ac4de10bb4ab45d5faf3
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-13 17:15:35 +0530

    Add files via upload

---
+++ Multiple.java
@@ -0,0 +1,13 @@
+package set1;
+import java.util.*;
+public class Set1 {
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int n,mul;
+n=in.nextInt();
+for(int i=1;i<=n;i++){
+	mul=i*n;
+	System.out.print(mul+" ");
+}
+}
+}
