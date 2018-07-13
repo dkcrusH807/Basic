86ab97b3bb4cfdb42694b910c332473063f6cf53
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-13 11:01:05 +0530

    Add files via upload

--- even.java
+++ even.java
@@ -2,11 +2,13 @@ package set1;
 import java.util.*;
 public class Set1 {
 public static void main(String[] args) {
-	int N;
-	Scanner in=new Scanner(System.in);
-	N=in.nextInt();
-	if(N%2==0||N==0){
-		System.out.println("Even");
-	}else{
-System.out.println("Odd");
+Scanner in=new Scanner(System.in);
+int n,q,temp=0;
+n=in.nextInt();
+q=in.nextInt();
+for(int i=n+1;i<q;i++){
+	if(i%2==0)
+System.out.println(i);
+}
+}
 }
