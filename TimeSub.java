943639fa1e310699fc80700a746b7a43397c15f8
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-14 17:31:21 +0530

    Add files via upload

---
+++ TimeSub.java
@@ -0,0 +1,14 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+int h1,m1,h2,m2;
+Scanner in=new Scanner(System.in);
+h1=in.nextInt();
+m1=in.nextInt();
+h2=in.nextInt();
+m2=in.nextInt();
+System.out.println((h1-h2)+" "+(m1-m2));
+}
+}
+
