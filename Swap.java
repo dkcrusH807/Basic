8d5b66fe7be07ded5183a74add97a2308a7a889c
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-15 12:22:44 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,17 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+int a,b;
+Scanner in=new Scanner(System.in);
+a=in.nextInt();
+b=in.nextInt();
+int temp;
+temp=a;
+a=b;
+b=temp;
+System.out.println(a+" "+b);
+}
+}
+
+
