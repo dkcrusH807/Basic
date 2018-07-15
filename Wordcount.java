cbb20cc087d7fc5ac4e7ab94186cce9fae723e7e
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-15 10:52:18 +0530

    Add files via upload

---
+++ Wordcount.java
@@ -0,0 +1,11 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+String str;
+Scanner in=new Scanner(System.in);
+str=in.nextLine();
+String []str1=str.split(" ");
+System.out.println(str1.length);
+}
+}
