648044b24f23c082e3a8fd7d5edbda479d886817
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-16 12:18:28 +0530

    Add files via upload

---
+++ Strcat.java
@@ -0,0 +1,13 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+String s,s1;
+Scanner in=new Scanner(System.in);
+s=in.nextLine();
+s1=in.nextLine();
+System.out.println(s.concat(s1));
+}
+}
+
+
