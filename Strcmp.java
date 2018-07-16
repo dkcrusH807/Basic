69d906f03281a508a0001ded6270480117859e50
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-16 12:15:18 +0530

    Add files via upload

---
+++ Strcmp.java
@@ -0,0 +1,18 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+String s,s1;
+int n;
+Scanner in=new Scanner(System.in);
+s=in.nextLine();
+s1=in.nextLine();
+if(s.length()>s1.length()||s.length()==s.length()){
+	System.out.println(s);
+}else{
+	System.out.println(s1);
+}
+}
+}
+
+
