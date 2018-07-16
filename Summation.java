1d9ae579ea5b5e69d953f7baa05650ab2b3a1409
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-16 14:05:59 +0530

    Add files via upload

---
+++ Summation.java
@@ -0,0 +1,12 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int a,b;
+a=in.nextInt();
+b=in.nextInt();
+System.out.println(a+b);
+}
+}
+
