8ecba49d08ca77210dc5f47fb4c8f57755f244b0
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-16 14:00:09 +0530

    Add files via upload

---
+++ Averge.java
@@ -0,0 +1,18 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int n,sum=0;
+n=in.nextInt();
+int arr[]=new int[n];
+for(int i=0;i<n;i++){
+	arr[i]=in.nextInt();
+}
+for(int i=0;i<n;i++){
+	sum=sum+arr[i];
+}
+System.out.println(sum/n);
+}
+}
+
