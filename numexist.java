903ca84f331a6f271dee74f3a4ccd039fc5c3103
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-16 15:21:59 +0530

    Add files via upload

---
+++ numexist.java
@@ -0,0 +1,23 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int n,n1,count=0;
+n=in.nextInt();
+n1=in.nextInt();
+int arr[]=new int[n];
+for(int i=0;i<n;i++){
+	arr[i]=in.nextInt();
+}
+for(int i=0;i<n;i++){
+	if(arr[i]==n1)
+		count=1;
+}if(count==1)
+System.out.println("yes");
+else
+	System.out.println("no");
+}
+}
+
+
