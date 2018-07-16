e2c6bb7eae1c202592c32c7120711222e97fd120
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-16 14:22:45 +0530

    Add files via upload

---
+++ Order.java
@@ -0,0 +1,13 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int a,b,c;
+a=in.nextInt();
+b=in.nextInt();
+c=in.nextInt();
+System.out.print(a+" "+b+" "+c+" ");
+}
+}
+
