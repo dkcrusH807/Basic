17e17eddb9a8cd2ceff822443523b2c83b09c029
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-16 15:41:43 +0530

    Add files via upload

---
+++ Stringchar.java
@@ -0,0 +1,14 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+String s;
+int n;
+s=in.nextLine();
+n=in.nextInt();
+System.out.println(s.substring(0,n));
+}
+}	
+
+
