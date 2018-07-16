10b8b31387d58caea6436dd756c7a80db08439e7
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-16 14:44:48 +0530

    Add files via upload

---
+++ numword.java
@@ -0,0 +1,30 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int n;
+n=in.nextInt();
+if(n==1)
+	System.out.println("one");
+if(n==2)
+	System.out.println("two");
+if(n==3)
+	System.out.println("three");
+if(n==4)
+	System.out.println("four");
+if(n==5)
+	System.out.println("five");
+if(n==6)
+	System.out.println("six");
+if(n==7)
+	System.out.println("seven");
+if(n==8)
+	System.out.println("eight");
+if(n==9)
+	System.out.println("nine");
+if(n==10)
+	System.out.println("ten");
+}
+}
+
