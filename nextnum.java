443e799246f99dcc3d1e7a789a4029c6e33e290d
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-16 13:46:48 +0530

    Add files via upload

---
+++ nextnum.java
@@ -0,0 +1,11 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+int n;
+Scanner in=new Scanner(System.in);
+n=in.nextInt();
+System.out.println(n+1);
+}
+}
+
