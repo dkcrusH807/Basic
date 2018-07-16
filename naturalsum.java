c79da9150fefccbe4e373ba482c155a7adf26190
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-16 15:37:24 +0530

    Add files via upload

---
+++ naturalsum.java
@@ -0,0 +1,15 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int n,sum=0;
+n=in.nextInt();
+for(int i=1;i<=n;i++){
+	sum=sum+i;
+}
+System.out.println(sum);
+}
+}	
+
+
