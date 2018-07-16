d98723a94b9cf2b8157987b04f629540be17cf1e
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-16 14:52:46 +0530

    Add files via upload

---
+++ truncate.java
@@ -0,0 +1,14 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int n;
+n=in.nextInt();
+if(n%2==0){
+	System.out.println(n);
+}else if(n%2!=0){
+System.out.println(n-1);	
+}
+}
+}
