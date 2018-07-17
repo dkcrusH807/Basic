e08372e61d792f3d479ce58075b6589957d3549b
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-17 15:49:40 +0530

    Add files via upload

---
+++ DECIMAL_5.java
@@ -0,0 +1,12 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+float l,b;
+l=in.nextFloat();
+b=in.nextFloat();
+double a=l*b;
+System.out.format("%.5f",a);
+}
+}
