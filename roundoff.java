89f66ac6e53023d3d593f3fad782f28ac4b54ee6
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-17 15:21:12 +0530

    Add files via upload

---
+++ roundoff.java
@@ -0,0 +1,10 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+float n;
+n=in.nextFloat();
+System.out.println(Math.round(n)+1);
+}
+}
