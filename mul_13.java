fb304a3930658dd7c3f7e190d83b7c945e3e90c9
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-17 14:05:05 +0530

    Add files via upload

---
+++ mul_13.java
@@ -0,0 +1,13 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+int n;
+Scanner in=new Scanner(System.in);
+n=in.nextInt();
+if(n%13==0)
+System.out.print("yes");
+else
+	System.out.println("no");
+}
+}
