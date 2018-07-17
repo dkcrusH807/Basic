505c652c806f7c3f2c6891e3fe020ae5dd148dce
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-17 16:58:23 +0530

    Add files via upload

---
+++ Ascii_ch.java
@@ -0,0 +1,10 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+char ch;
+ch=in.next().charAt(0);
+System.out.println((int)ch);
+}
+}
