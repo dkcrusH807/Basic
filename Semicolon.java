7130d31dbfcc391006fcc90c7632b14735881b00
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-17 09:13:14 +0530

    Add files via upload

---
+++ Semicolon.java
@@ -0,0 +1,10 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+if(System.out.printf("Kavin mani")==null){
+}
+}
+}
+
