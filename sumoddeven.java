a200f47e64f8cf2128a62341d5408470ec430a66
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-17 09:19:18 +0530

    Add files via upload

---
+++ sumoddeven.java
@@ -0,0 +1,15 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int a,b;
+a=in.nextInt();
+b=in.nextInt();
+if((a+b)%2==0)
+System.out.println("even");
+else
+System.out.println("odd");
+}
+}
+
