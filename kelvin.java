812e2421151a225674921053086febd761bf6b30
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-18 14:14:01 +0530

    Add files via upload

---
+++ kelvin.java
@@ -0,0 +1,10 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+float t;
+Scanner in=new Scanner(System.in);
+t=in.nextFloat();
+System.out.println(t+273);
+}
+}
