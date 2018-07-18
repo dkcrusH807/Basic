c5a6e8be690c21a8eaa2260f6aecd2e3201fd185
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-18 14:33:34 +0530

    Add files via upload

---
+++ SimpleInteres.java
@@ -0,0 +1,13 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in); 
+double p;
+float r,t;
+p=in.nextDouble();
+r=in.nextFloat();
+t=in.nextFloat();
+System.out.println(((p*r*t)/100));
+}
+}
