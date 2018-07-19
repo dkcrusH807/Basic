540791f021d2c4e7663d27881ba28ec581ca5682
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-19 10:13:06 +0530

    Add files via upload

---
+++ Roman.java
@@ -0,0 +1,49 @@
+package List;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+String s;
+Scanner in=new Scanner(System.in);
+s=in.nextLine();
+if(s.equals("I"))
+	System.out.println("1");
+if(s.equals("II"))
+	System.out.println("2");
+if(s.equals("III"))
+	System.out.println("3");
+if(s.equals("IV"))
+	System.out.println("4");
+if(s.equals("V"))
+	System.out.println("5");
+if(s.equals("VI"))
+	System.out.println("6");
+if(s.equals("VII"))
+	System.out.println("7");
+if(s.equals("VIII"))
+	System.out.println("8");
+if(s.equals("IX"))
+	System.out.println("9");
+if(s.equals("X"))
+	System.out.println("10");
+if(s.equals("XI"))
+	System.out.println("11");
+if(s.equals("XII"))
+	System.out.println("12");
+if(s.equals("XIII"))
+	System.out.println("13");
+if(s.equals("XIV"))
+	System.out.println("14");
+if(s.equals("XV"))
+	System.out.println("15");
+if(s.equals("XVI"))
+	System.out.println("16");
+if(s.equals("XVII"))
+	System.out.println("17");
+if(s.equals("XVIII"))
+	System.out.println("18");
+if(s.equals("XIX"))
+	System.out.println("19");
+if(s.equals("XX"))
+	System.out.println("20");
+}
+}
