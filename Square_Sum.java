6bb76b9e75b810dd261a00db39bfd712720a5a86
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-19 14:52:31 +0530

    Add files via upload

---
+++ Square_Sum.java
@@ -0,0 +1,15 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int n,r,sum=0;
+n=in.nextInt();
+while(n!=0){
+r=n%10;
+sum=sum+(r*r);	
+n=n/10;
+}
+System.out.println(sum);
+}}
+
