fe87c5ba87c1a49c1e77a57e71a2c80b57746b81
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-19 10:05:50 +0530

    Add files via upload

---
+++ AppendStr.java
@@ -0,0 +1,10 @@
+package List;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+String str;
+Scanner in=new Scanner(System.in);
+str=in.nextLine();
+System.out.println(str+".");
+}
+}
