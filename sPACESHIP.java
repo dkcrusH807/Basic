2db63176005a1cffd5e518b4a1285bb27a1bd044
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-20 15:19:57 +0530

    Add files via upload

---
+++ sPACESHIP.java
@@ -0,0 +1,14 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+String str;
+str=in.nextLine();
+String s=str.replaceAll(" ","8");
+s=s.replaceAll("8","");
+System.out.println(s); 
+}
+}
+
+
