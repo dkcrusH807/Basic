9f2c060d40156a1da4e8de19b34cb4dfcd120106
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-20 15:17:39 +0530

    Add files via upload

---
+++ ResPACE.java
@@ -0,0 +1,14 @@
+package sql;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+String str;
+str=in.nextLine();
+String s=str.replaceAll(" ","4");
+s=s.replaceAll("4","");
+System.out.println(s); 
+}
+}
+
+
