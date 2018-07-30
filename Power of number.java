44e1eae5238d7025e4e27da861334e5f1b4c2717
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-30 17:39:52 +0530

    Add files via upload

---
+++ Power of number.java
@@ -0,0 +1,17 @@
+package sql;
+import java.util.*;
+public class guvi{
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int n,n1;
+n=in.nextInt();
+n1=in.nextInt();
+if(n%n1==0)
+System.out.println("yes");
+else{
+	System.out.println("no");
+}
+}
+}
+
+
