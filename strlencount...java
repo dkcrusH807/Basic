dec21b89f4df0c382e9dba862a28bad719c4c804
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-07-31 15:28:00 +0530

    Add files via upload

---
+++ strlencount...java
@@ -0,0 +1,15 @@
+package sql;
+import java.util.*;
+public class guvi{
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+String str;
+int count=0;
+str=in.nextLine();
+for(int i=0;i<str.length();i++){
+	count++;
+}
+System.out.println(count);
+}
+}
+
