f7f68d115775db5436b9b0e5ab6dd6a14f41afca
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-08-01 15:22:41 +0530

    Add files via upload

---
+++ Math.Power.java
@@ -0,0 +1,13 @@
+package sql;
+import java.util.*;
+public class guvi{
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int a,b;
+a=in.nextInt();
+b=in.nextInt();
+System.out.println(Math.pow(a, b));
+}
+}
+
+
