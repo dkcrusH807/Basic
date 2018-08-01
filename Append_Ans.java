b70d282d390179c8381e357f93f9a927d7603ddc
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-08-01 14:38:21 +0530

    Add files via upload

---
+++ Append_Ans.java
@@ -0,0 +1,13 @@
+package sql;
+import java.util.*;
+public class guvi{
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+String s;
+s=in.nextLine();
+StringBuffer s1=new StringBuffer(s+" ");
+System.out.println(s1.append("Answer"));
+}
+}
+
+
