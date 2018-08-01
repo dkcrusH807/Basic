1ce2e269b6e29ac44f2ae870179e9b29471a2524
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-08-01 14:13:35 +0530

    Add files via upload

---
+++ multiple_factorial.java
@@ -0,0 +1,15 @@
+package sql;
+import java.util.*;
+public class guvi{
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int n,f=1;
+n=in.nextInt();
+for(int i=1;i<=n;i++){
+	f=f*i;
+}
+System.out.println(f);
+}
+}
+
+
