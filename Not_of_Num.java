f81c6a6428742bffdc814897641a359c299b2e2c
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-08-02 11:50:01 +0530

    Add files via upload

---
+++ Not_of_Num.java
@@ -0,0 +1,13 @@
+package sql;
+import java.util.*;
+public class guvi{
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int num;
+num=in.nextInt();
+System.out.println(~num);
+}
+}
+
+
+
