d60dfc3e2dbd42919b74526b54557c531d600586
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-08-02 11:58:01 +0530

    Add files via upload

---
+++ leftshift_k.java
@@ -0,0 +1,14 @@
+package sql;
+import java.util.*;
+public class guvi{
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int num,k;
+num=in.nextInt();
+k=in.nextInt();
+System.out.println(num<<k);
+}
+}
+
+
+
