fc984c506da3ef27ef3d34046fa1062780c133a2
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-08-06 09:39:20 +0530

    Add files via upload

---
+++ decimal_bin.java
@@ -0,0 +1,9 @@
+package sql;
+import java.util.*;
+public class guvi{
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int n=in.nextInt();
+System.out.println(Integer.toBinaryString(n));
+}
+}
