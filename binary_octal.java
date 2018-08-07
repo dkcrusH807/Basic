5f2dbfed31d15a1e97d21208102ba4be4991d02d
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-08-07 12:18:34 +0530

    Add files via upload

---
+++ binary_octal.java
@@ -0,0 +1,11 @@
+package sql;
+import java.util.*;
+public class guvi{
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+String s;
+System.out.println(Integer.parseInt(in.nextLine(),2));
+int n=Integer.parseInt(in.nextLine(),2);
+System.out.println(Integer.toOctalString(n));
+}
+}
