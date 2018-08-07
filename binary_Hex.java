ed8b1cd72cfeba7cc8c52dd599eae8a8ac919e50
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-08-07 12:26:21 +0530

    Add files via upload

---
+++ binary_Hex.java
@@ -0,0 +1,10 @@
+package sql;
+import java.util.*;
+public class guvi{
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+String s;
+int n=Integer.parseInt(in.nextLine(),2);
+System.out.println(Integer.toHexString(n));
+}
+}
