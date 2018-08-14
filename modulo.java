b5613a6c3de5ea53345a546082146f7ca63e2b3e
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-08-14 15:18:37 +0530

    Add files via upload

---
+++ modulo.java
@@ -0,0 +1,12 @@
+package github;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int a,b,c;
+a=in.nextInt();
+b=in.nextInt();
+c=in.nextInt();
+System.out.println(Math.pow(a,b)%c);
+}
+}
