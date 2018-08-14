02b72cc22fdb53c7b647379e27d5652b9d650339
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-08-14 15:10:52 +0530

    Add files via upload

---
+++ length_binstr.java
@@ -0,0 +1,10 @@
+package github;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int hex;
+hex=in.nextInt();
+System.out.println(Integer.toBinaryString(hex).length());
+}
+}
