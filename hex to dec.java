58373e83c9e66a9a7e642824d52fcf66e7245070
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-08-14 15:07:03 +0530

    Add files via upload

---
+++ hex to dec.java
@@ -0,0 +1,10 @@
+package github;
+import java.util.*;
+public class Guvi {
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+String hex;
+hex=in.nextLine();
+System.out.println(Integer.parseInt(hex,16));
+}
+}
