cfbed2a799809db635c770bdf8ba6367a500f2c5
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-08-20 10:06:49 +0530

    Add files via upload

---
+++ all_divisible.java
@@ -0,0 +1,26 @@
+package guvi;
+import java.util.*;
+public class PossibleChar {
+public static void main(String args[]){
+Scanner in=new Scanner(System.in);
+int n,n1=1000000,count=0;
+n=in.nextInt();
+int arr[]=new int[n];
+for(int i=0;i<n;i++){
+	arr[i]=in.nextInt();
+}
+for(int i=1;i<=1000000;i++){
+	count=0;
+	for(int j=0;j<n;j++){
+		if(i%arr[j]==0)
+			count++;
+	}
+	if(count==n){
+		if(i<n1)
+			n1=i;
+		
+	}
+}
+System.out.println(n1);
+}
+}
