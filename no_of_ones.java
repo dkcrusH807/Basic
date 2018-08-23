033d75d2336295a67732e43501371f2668b11c7e
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-08-23 09:23:50 +0530

    Add files via upload

---
+++ no_of_ones.java
@@ -0,0 +1,20 @@
+package sql;
+import java.util.*;
+public class guvi{
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int n,count=0;
+n=in.nextInt();
+String str=Integer.toBinaryString(n),str2="";
+for(int i=str.length()-1;i>=0;i--){
+     str2=str2+str.charAt(i);
+}
+System.out.println(str2);
+for(int i=0;i<str2.length();i++){
+	if(str2.substring(i,i+1).equals("1")){
+		count++;
+	}
+}
+System.out.println(count);
+}
+}
