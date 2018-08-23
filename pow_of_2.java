d52d7613d4a1b3e9e4c9472eb308d8c88e06e329
dkcrusH <40513849+dkcrusH807@users.noreply.github.com>
2018-08-23 09:20:05 +0530

    Add files via upload

---
+++ pow_of_2.java
@@ -0,0 +1,14 @@
+package sql;
+import java.util.*;
+public class guvi{
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int n;
+n=in.nextInt();
+if(n%2==0){
+	System.out.println("Yes");
+}else{
+	System.out.println("No");
+}
+}
+}
